4958bc83db50a8109e4840049050a92097ee58fe
NamitaUpadhye <upadhyenamita78@gmail.com>
2026-04-19 20:30:36 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,12 @@
+package lab3b;
+public class Q4 {
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam"));
+    }
+}
