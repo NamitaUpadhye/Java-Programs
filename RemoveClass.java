262c3e1dd92a19cfaff04ce5c8a12cf2d3c34422
NamitaUpadhye <upadhyenamita78@gmail.com>
2026-04-19 15:11:42 +0530

    Add files via upload

---
+++ RemoveClass.java
@@ -0,0 +1,17 @@
+package lab1b;
+import java.util.*;
+
+public class RemoveClass {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.remove(1);
+        colors.remove("Blue");
+
+        System.out.println(colors);
+    }
+}
