44cfed584a59d23d657866579f2e4379832134b4
NamitaUpadhye <upadhyenamita78@gmail.com>
2026-04-19 18:24:32 +0530

    Add files via upload

---
+++ List1.java
@@ -0,0 +1,17 @@
+package lab1cLinkedList;
+import java.util.*;
+public class List1 {
+	    public static void main(String[] args) {
+	        LinkedList<String> listobj = new LinkedList<>();
+	        listobj.add("Red");
+	        listobj.add("Blue");
+	        listobj.add("Green");
+	        listobj.add("Yellow");
+	        Iterator<String> iteratorobj = listobj.listIterator(1);
+	        System.out.println("Elements from 2nd position:");
+	        while (iteratorobj.hasNext()) {
+	            System.out.println(iteratorobj.next());
+	        }
+	    }
+	}
+
